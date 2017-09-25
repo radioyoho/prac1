63ac74912b8157fb3fd08305312fd5f8f2eedbfc
santiago <31044042+SantSou@users.noreply.github.com>
2017-09-25 11:15:54 -0500

    fixed mistakes in $ra
    move function now working, conflicts with base case

--- pract2.2.asm
+++ pract2.2.asm
@@ -1,7 +1,7 @@
 .data
 .text
 	addi $t7, $zero, 1	#temporary to store 1
-	addi $s0,$zero, 5	#number of discs
+	addi $s0,$zero, 3	#number of discs
 	add $a0, $zero, $s0	#copy to a0
 	lui $s1,0x1001		#Upper adress
 	lui $a1,0x1001		#upper adress
@@ -19,14 +19,16 @@ fill: 		#initialize all towers in memory, according to N discs
 	jal hanoi					#call hanoi, (N,ORIGIN, DESTINY, AUX)
 	j exit			
 	#</init>
-hanoi:	addi $sp,$sp,-4
+hanoi:	addi $sp,$sp,-8
 	sw $ra,0($sp)		#back up Register Address
+	sw $a0,4($sp)		#back up Register Address
 		beq $a0, $t7, base	#if n>1 goes to recursion
 		jal recursion1		#calls to switch aux and destiny
 		jal move1		#makes the move
 		jal recursion2		#calls to switch aux and origin
-	bret: lw $ra,0($sp)
-	addi $sp,$sp,4		#returns to RA
+bret:   lw $ra,0($sp)
+	lw $a0,4($sp)
+	addi $sp,$sp,8		#returns to RA
 	jr $ra			#from where it was called
 
 base:	add $s1,$a1,$zero
@@ -40,59 +42,69 @@ baseMul: addi, $s1,$s1,4	#counts 4 n times
 	addi $t0,$t0,1		#j++
 	j b2
 	
-recursion1:	addi $a0, $a0, -1	#height param -1
-		add $t0, $zero, $a2	#switch dest with aux
-		add $a2, $zero, $a3	
-		add $a3, $zero, $t0
-		addi $sp, $sp, -20	#backup temporaries
+recursion1:	addi $sp, $sp, -20	#backup temporaries
 			sw $a1, 0($sp)
 			sw $a2, 4($sp)
 			sw $a3, 8($sp)	
 			sw $ra, 12($sp)
+			sw $a0, 16($sp)
+		addi $a0, $a0, -1	#height param -1
+		add $t0, $zero, $a2	#switch dest with aux
+		add $a2, $zero, $a3	
+		add $a3, $zero, $t0
 		jal hanoi		#call hanoi,(N-1,ORIGIN, AUX, DESTINY)
 			lw $a1, 0($sp)
 			lw $a2, 4($sp)
 			lw $a3, 8($sp)	
 			lw $ra, 12($sp)
+			lw $a0, 16($sp)
 		addi $sp, $sp, 20
 		jr $ra
 
-move1:	addi $sp,$sp,-4
+move1:	addi $sp,$sp,-4		#move disks in memory function
 	sw $ra,0($sp)		#back up Register Address
-		add $t6,$zero,$a1
-		lui $t5,0x1001	
-		jal removetopdisk
-		add $t6,$zero,$a3
-		lui $t5,0x1001
-		jal addtopdisk
+		add $t6,$zero,$a1	#load as temporary the origin tower address
+		add $t5,$zero,$zero	#initialize a counter	
+		jal removetopdisk	#remove the top disk
+		add $t6,$zero,$a3	#load as temporary the destiny tower address
+		add $t5,$zero,$zero	#Re-initialize a counter
+		jal addtopdisk		#add a disk on top of destiny
 	lw $ra,0($sp)
 	addi $sp,$sp,4		#returns to RA	
 	jr $ra	
-addtopdisk:	lw $t4,0($t6)
-		beq $t4,$t5,retadd 
-		j addtopdisk
-retadd:		sw $t7,0($t6)
+addtopdisk:	lw $t4,0($t6)		#loads in t4 pos 0 of origin tower
+		beq $t4,$t5,retadd	#compares pos 0 with zero
+		addi $t6,$t6,4		#jumps to next position in tower
+		j addtopdisk		#goes to loop
+retadd:		sw $t2,0($t6)
 		jr $ra	
-removetopdisk:	lw $t4,0($t6)
+		
+removetopdisk:	lw $t4,0($t6)		#loads in t4 pos 0 of origin tower
 		beq $t4,$t5,retremove 
-		j addtopdisk
-retremove:	sw $zero,0($t6)
+		addi $t6,$t6,4		#jumps to next position in tower
+		j removetopdisk
+retremove:	addi $t6,$t6,-4		#returns to pos-1 (top disk)
+		lw $t2,0($t6)		#stores top disk in t2
+		sw $zero,0($t6)		#removes top disk from origin
 		jr $ra
 		
-recursion2:	addi $a0, $a0, -1	#height param -1
-		add $t0, $zero, $a1	#switch dest with aux
-		add $a1, $zero, $a2	
-		add $a2, $zero, $t0
-		addi $sp, $sp, -16	#backup temporaries
+recursion2:	addi $sp, $sp, -20	#backup temporaries
 			sw $a1, 0($sp)
 			sw $a2, 4($sp)
 			sw $a3, 8($sp)	
 			sw $ra, 12($sp)
+			sw $a0, 16($sp)
+		addi $a0, $a0, -1	#height param -1
+		add $t0, $zero, $a1	#switch dest with aux
+		add $a1, $zero, $a2	
+		add $a2, $zero, $t0
+		
 		jal hanoi		#call hanoi,(N-1,AUX, DESTINY, ORIGIN)
 			lw $a1, 0($sp)
 			lw $a2, 4($sp)
 			lw $a3, 8($sp)	
 			lw $ra, 12($sp)
-		addi $sp, $sp, 16
+			lw $a0, 16($sp)
+		addi $sp, $sp, 20
 		jr $ra	
 exit:	#end of program
